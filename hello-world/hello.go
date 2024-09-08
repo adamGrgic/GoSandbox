27c0c3784c345e35d1b02de1671eb51017204b96
Adam Grgić <adam@grgicconsulting.com>
2024-09-07 21:08:58 -0600

    project init

---
+++ hello-world/hello.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// starter hello world code 
+// func main() {
+//     fmt.Println("Hello, World!")
+// }
+
+// starter code from sample module
+import "rsc.io/quote"
+
+func main() {
+    fmt.Println(quote.Go())
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+Hello-World
+- most bare bones basic setup of a program 
+- execute a file from the command line
+
+Create-Module
+- use module to call one function in one file from another
+
