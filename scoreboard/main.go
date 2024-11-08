aaf3b1885397fd0ffbf498674eac182de1d3063c
Adam Grgić <adam@grgicconsulting.com>
2024-11-07 23:35:51 -0700

    update

--- scoreboard/main.go
+++ scoreboard/main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"embed"
 	"net/http"
 )
 
-//go:embed static/*
-var staticFiles embed.FS
-
 func main() {
-	fs := http.FS(staticFiles)
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	http.ListenAndServe(":8080", nil)
 }
